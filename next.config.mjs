76496c5659f08e801ce5936a66352340c0bcfbdc
SAHIN MALLIK <86676300+sahinmallik@users.noreply.github.com>
2025-04-12 17:29:42 +0530

    Config file updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
-export default {
+const nextConfig = {
   experimental: {
-    turbopack: true,
+    turbopack: true, // enable Turbopack for development
   },
   images: {
     remotePatterns: [
@@ -15,26 +15,6 @@ export default {
       },
     ],
   },
-  webpack: async (config) => {
-    // Custom Webpack configuration for Turbopack support
-    const webpack = await import("webpack");
-
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      crypto: await import("crypto-browserify"),
-      stream: await import("stream-browserify"),
-      assert: await import("assert"),
-      buffer: await import("buffer"),
-      process: await import("process/browser"),
-    };
-
-    config.plugins.push(
-      new webpack.default.ProvidePlugin({
-        process: ["process/browser"],
-        Buffer: ["buffer", "Buffer"],
-      })
-    );
-
-    return config;
-  },
 };
+
+export default nextConfig;
