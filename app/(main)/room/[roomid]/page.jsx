3664e6aac34d7337beb161b21510c10c25a04edb
SAHIN MALLIK <86676300+sahinmallik@users.noreply.github.com>
2025-04-15 16:17:29 +0530

    next config file being changed

--- app/(main)/room/[roomid]/page.jsx
+++ app/(main)/room/[roomid]/page.jsx
@@ -1,9 +1,13 @@
-import ZegoRoomClient from "@/components/ZegoRoomClient";
-import { use } from "react";
+"use client";
+
+import dynamic from "next/dynamic";
+
+const ZegoRoomClient = dynamic(() => import("@/components/ZegoRoomClient"), {
+  ssr: false,
+});
 
 const RoomPage = ({ params }) => {
-  const { roomid } = use(params);
-
+  const { roomid } = params;
   return <ZegoRoomClient roomid={roomid} />;
 };
 
